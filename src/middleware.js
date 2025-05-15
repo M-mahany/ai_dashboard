69fda6ea072bdcb8120e9af0d3cdd7c5b59c474b
Mena Mahany <mahanymena@gmail.com>
2025-05-15 23:02:20 +0300

    improved middleware for aws amplify

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,12 @@
 import { NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
 
 // 1. Specify protected and public routes
 const protectedRoutes = '/dashboard';
 const authRoutes = '/auth';
+
+const getAuthToken = (request) => {
+  return request.cookies.get('authToken') || null;
+};
 
 export default async function middleware(req) {
   // 2. Check if the current route is protected or public
@@ -12,16 +15,16 @@ export default async function middleware(req) {
   const isAuthRoutes = path.startsWith(authRoutes); // same as /auth/*
 
   // 3. Decrypt the session from the cookie
-  const authToken = req.cookies.get('authToken')?.value;
+  const authToken = getAuthToken(req);
 
   // 5. Redirect to /login if the user is not authenticated
   if (isProtectedRoute && !authToken) {
-    return NextResponse.redirect(new URL('/auth/login', req.nextUrl));
+    return NextResponse.redirect(new URL('/auth/login', req.nextUrl.origin));
   }
 
   // 6. Redirect to /dashboard if the user is authenticated
   if (isAuthRoutes && authToken) {
-    return NextResponse.redirect(new URL('/dashboard/recordings', req.nextUrl));
+    return NextResponse.redirect(new URL('/dashboard/recordings', req.nextUrl.origin));
   }
 
   return NextResponse.next();
